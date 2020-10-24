3dc3fb6936425b7ed157d54b351c4db6f0a76c5a
Luqman Akram <luqmanakram00300@outlook.com>
2020-10-23 23:26:11 -0700

    Add project files.

---
+++ Api/Api/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Api.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Api
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new YoutubePage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
